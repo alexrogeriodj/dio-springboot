03befb3693d8fa868f5bf3e1440b0f97df7866f1
Gleyson Sampaio <glysns@hotmail.com>
2021-09-07 07:36:59 -0300

    fim

--- spring-data-jpa-repository/src/main/java/dio/springboot/app/SistemaUsuario.java
+++ spring-data-jpa-repository/src/main/java/dio/springboot/app/SistemaUsuario.java
@@ -1,5 +1,6 @@
 package dio.springboot.app;
 
+import org.hibernate.internal.util.collections.JoinedIterable;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -15,12 +16,34 @@ public class SistemaUsuario implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
+		if(!repository.existsByLogin("gleyson")) {
+			Usuario gleyson = criarUsuarioGleyson();
+			repository.save(gleyson);
+		}
+		if(!repository.existsByLogin("admin")) {
+			Usuario admin = criarUsuarioAdmin();
+			repository.save(admin);
+		}
+		
+		Iterable<Usuario> usuarios = repository.findAll();
+		usuarios.forEach(u->System.out.println(u.getNomeCompleto() + " - " + u.getLogin()));
+		
+	}
+	private Usuario criarUsuarioGleyson() {
 		Usuario usuario = new Usuario();
 		usuario.setEmail("gleyson@dio.com.br");
 		usuario.setLogin("gleyson");
 		usuario.setNomeCompleto("GLEYSON SAMPAIO");
 		usuario.setSenha("diospringjpa");
-		repository.save(usuario);
-		
+		return usuario;
 	}
+	private Usuario criarUsuarioAdmin() {
+		Usuario usuario = new Usuario();
+		usuario.setEmail("admin@dio.com.br");
+		usuario.setLogin("admin");
+		usuario.setNomeCompleto("ADMINISTRADOR");
+		usuario.setSenha("masterpass");
+		return usuario;
+	}
+	
 }
--- spring-data-jpa-repository/src/main/java/dio/springboot/app/repository/UsuarioRepository.java
+++ spring-data-jpa-repository/src/main/java/dio/springboot/app/repository/UsuarioRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.repository.CrudRepository;
 import dio.springboot.app.model.Usuario;
 
 public interface UsuarioRepository extends CrudRepository<Usuario, Integer> {
-
+	boolean existsByLogin(String login);
 }
