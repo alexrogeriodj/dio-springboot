8b0e59763d5eee6f312efc7c7a555da0491c233a
Gleyson Sampaio <glysns@hotmail.com>
2021-09-14 14:26:36 -0300

    repository

--- springboot-web-swagger/src/main/java/dio/springbootweb/CrudController.java
+++ springboot-web-swagger/src/main/java/dio/springbootweb/CrudController.java
@@ -1,4 +1,4 @@
-package dio.springbootweb.controller;
+package dio.springbootweb;
 
 import dio.springbootweb.model.Cliente;
 import dio.springbootweb.repository.ClienteRepository;
@@ -8,27 +8,27 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-public class ClienteController {
-    @Autowired
-    private ClienteRepository repository;
+public class CrudController {
 
-    @PostMapping("/clientes")
-    public void post(@RequestBody Cliente cliente){
-        repository.save(cliente);
+    @PostMapping("/crud/post")
+    public void post(@RequestBody User user){
+        System.out.println("POST - Este foi o usuário recebido");
+
     }
-    @PutMapping("/clientes")
-    public void put(@RequestBody Cliente cliente){
-        repository.update(cliente);
+    @PutMapping("/crud/put")
+    public void put(@RequestBody User user){
+        System.out.println("PUT - Este foi o usuário recebido");
+        System.out.println(user);
     }
-    @GetMapping("/clientes")
+    @GetMapping("/crud/all")
     public List<Cliente> getAll(){
         return repository.listAll();
     }
-    @GetMapping("/clientes/{id}")
+    @GetMapping("/crud/one/{id}")
     public Cliente getOne(@PathVariable("id") Integer id){
         return repository.finById(id);
     }
-    @DeleteMapping("/clientes/{id}")
+    @DeleteMapping("/crud/delete/{id}")
     public void delete(@PathVariable("id") Integer id){
         repository.remove(id);
     }
---
+++ springboot-web-swagger/src/main/java/dio/springbootweb/UsuarioRepository.java
@@ -0,0 +1,8 @@
+package dio.springbootweb;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class UsuarioRepository {
+
+}
