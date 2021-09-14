8d078675ca794c7aeacbb3a1fbf7f4a915a205e2
Gleyson Sampaio <glysns@hotmail.com>
2021-09-14 06:54:29 -0300

    repository

--- springboot-web/src/main/java/dio/springbootweb/repository/ClienteRepository.java
+++ springboot-web/src/main/java/dio/springbootweb/repository/ClienteRepository.java
@@ -7,6 +7,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 @Repository
 public class ClienteRepository {
@@ -16,7 +17,8 @@ public class ClienteRepository {
 
     }
     public void save(Cliente cliente){
-        clientes.put(++nextId,cliente);
+        cliente.setId(++nextId);
+        clientes.put(cliente.getId(),cliente);
     }
     public void update(Cliente cliente){
         clientes.put(cliente.getId(),cliente);
@@ -25,7 +27,7 @@ public class ClienteRepository {
         clientes.remove(id);
     }
     public List<Cliente> listAll(){
-        return (List<Cliente>) clientes.values();
+        return clientes.values().stream().collect(Collectors.toList());
     }
     public Cliente finById(Integer id){
         return clientes.get(id);
