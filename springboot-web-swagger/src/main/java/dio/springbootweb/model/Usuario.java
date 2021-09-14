a44f0f56a709378d82f753d194c3705bd22def87
Gleyson Sampaio <glysns@hotmail.com>
2021-09-14 14:45:38 -0300

    swagger

--- springboot-web-swagger/src/main/java/dio/springbootweb/model/Usuario.java
+++ springboot-web-swagger/src/main/java/dio/springbootweb/model/Usuario.java
@@ -1,25 +1,25 @@
-package dio.springbootweb;
+package dio.springbootweb.model;
 
-public class User {
+public class Usuario {
     private String login;
     private String password;
-
+    public Usuario() {}
+    public Usuario(String login, String password) {
+        this.login = login;
+        this.password = password;
+    }
     public String getLogin() {
         return login;
     }
-
     public void setLogin(String login) {
         this.login = login;
     }
-
     public String getPassword() {
         return password;
     }
-
     public void setPassword(String password) {
         this.password = password;
     }
-
     @Override
     public String toString() {
         return "User{" +
